713cd43b5ae19fa0301e6bf13ec0a1d6b4d6a441
kauansddsdela <kauanacalmeida17@gmail.com>
2024-11-09 09:01:31 -0300

    primeiro commit

---
+++ test/example.test.js
@@ -0,0 +1,9 @@
+const assert = require("assert")
+
+describe('exempli de teste', () => {
+
+    it('deve somar dois numeros corretamente', () => {
+        assert.strictEqual(1 + 1, 2);
+    });
+
+})
